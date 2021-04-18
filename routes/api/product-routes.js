8d8731b9f849bb9f26854aa14ddd85e876dedf7a
Stephanie Freyler <stephaniefreylersantana@gmail.com>
2021-04-17 18:33:01 -0700

    updated category and product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
+// associations 
 
 // The `/api/products` endpoint
 
@@ -7,12 +8,62 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [
+      {
+        model: Category,
+        attributes: ["id", "category_name"],
+      },
+      {
+        model: Tag,
+        as: "tagged_products",
+      },
+    ],
+  })
+  .then((dbProductData) => {
+    if (!dbProductData) {
+      res.status(404).json({ message: "No Product found with this id" });
+      return;
+    }
+    res.json(dbProductData);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Category,
+        attributes: ["id", "category_name"],
+      },
+      {
+        model: Tag,
+        as: "tagged_products",
+      },
+    ],
+  })
+  .then((dbProductData) => {
+    if (!dbProductData) {
+      res.status(404).json({ message: "No Product found with this id" });
+      return;
+    }
+    res.json(dbProductData);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+
 });
 
 // create new product
@@ -91,6 +142,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    },
+  })
+  .then((dbCategoryData) => {
+    if (!dbCategoryData) {
+      res.status(404).json({ message: "No Category found with this id" });
+      return;
+    }
+    res.json(dbCategoryData);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
